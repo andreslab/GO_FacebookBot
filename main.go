b7a73fcb32e1f14363f954b723b8fa1966077b9c
Jaime Andrade <jaimeandrade454@gmail.com>
2017-11-03 21:23:06 -0500

    handle de petición que autentifica el acceso con el token que prove facebook

--- main.go
+++ main.go
@@ -8,9 +8,10 @@ import (
 )
 
 type Config struct {
-	Port string `json: "port"`
-	Cert string `json: "cert"`
-	Key  string `json: "key"`
+	Port    string `json:"port"`
+	CertPem string `json:"cert_pem"`
+	KeyPem  string `json:"key_pem"`
+	MyToken string `json:"my_token"`
 }
 
 var config Config
@@ -19,8 +20,11 @@ func main() {
 	loadConfig()
 
 	http.HandleFunc("/", saludar)
+	http.HandleFunc("fb", fbbot)
 
-	log.Printf("Servidor escuchando.... https://localhost:%s", config.Port)
+	log.Printf("Servidor escuchando.... https://localhost%s", config.Port)
+	println(config.CertPem)
+	println(config.KeyPem)
 	//http.ListenAndServe(":8080", nil)
 	//uso de https
 	/*err := http.ListenAndServeTLS(":443", "./certificates/cert.pem", "./certificates/key.pem", nil) //el handle es nil
@@ -28,7 +32,9 @@ func main() {
 		log.Println(err)
 	}*/
 
-	err := http.ListenAndServeTLS(config.Port, config.Cert, config.Key, nil) //el handle es nil
+	//err := http.ListenAndServeTLS(config.Port, config.Cert, config.Key, nil) //el handle es nil
+	err := http.ListenAndServeTLS(config.Port, config.CertPem, config.KeyPem, nil) //el handle es nil
+
 	if err != nil {
 		log.Println(err)
 	}
@@ -36,6 +42,20 @@ func main() {
 
 func saludar(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hola mundo"))
+}
+func fbbot(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		vt := r.URL.Query().Get("hub.verify_token")
+		if vt == config.MyToken {
+			hc := r.URL.Query().Get("hub.challenge")
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(hc))
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("token no valido"))
+		return
+	}
 }
 
 func loadConfig() {
